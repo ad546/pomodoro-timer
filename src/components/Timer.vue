9f12f5027733a0ad0f93da3604be8a4119f34ec7
ad546 <ad546github@gmail.com>
2018-06-27 06:56:43 -0500

    padding zeroes added, reverse timer for pomodoro added

--- src/components/Timer.vue
+++ src/components/Timer.vue
@@ -1,10 +1,11 @@
 <template>
     <div>
-        <p>{{ seconds }}</p>
+        <p>{{ hours }} : {{ minutes }} : {{ seconds }}</p>
         <hr>
         <button @click="startTimer">Start</button>
         <button @click="stopTimer">Stop</button>
         <button @click="resetTimer">Reset</button>
+        <button @click="pomodoroTime">Pomodoro Timer</button>
     </div>
 </template>
 
@@ -13,7 +14,10 @@ export default {
     data: function() {
         return {
             timeVar: null,
-            seconds: '0'
+            total: '3595',
+            minutes: '59',
+            hours: '0',
+            seconds: '55'
         }
     },
     methods: {
@@ -24,10 +28,37 @@ export default {
             clearInterval(this.timeVar);
         },
         resetTimer() {
-            this.seconds = 0;
+            this.seconds = '00';
+            this.minutes = '00';
+            this.hours = '0';
+            this.total = 0;
+        },
+        display(){
+            this.minutes = Math.floor(this.total / 60);
+            this.seconds = this.total - this.minutes * 60;
+            this.hours = Math.floor(this.total / 3600);
+            (this.seconds < 10 ? this.seconds = ('0' + this.seconds) : this.seconds = this.seconds);
+            (this.minutes > 59 ? this.minutes = 0 : this.minutes = this.minutes);
+            (this.minutes < 10 ? this.minutes = ('0' + this.minutes) : this.minutes = this.minutes);
         },
         setTime() {
-            this.seconds++;
+            this.total++;
+            this.display();
+        },
+        pomodoroTime(){
+            this.resetTimer();
+            this.total = 5;
+            this.display();
+            this.timeVar = setInterval(this.reverseSetTime, 1000);
+        },
+        reverseSetTime() {
+            this.total--;
+            this.display();
+            if (this.total === 0){
+                alert("Break Time!");
+                this.stopTimer();
+            }
+            
         }
     }
 }
